4d0c7a22c5233d370fc2619127b8d5806b90f621
Charqican <david_7valenzuela@hotmail.com>
2025-06-28 17:30:50 -0400

    update

--- proyecto/entrypoint.sh
+++ proyecto/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-# Inicializa la BD
-airflow db init
+# Inicializa DB si no existe
+if [ ! -f "${AIRFLOW_HOME}/airflow.db" ]; then
+    echo "===> airflow.db no existe, inicializando base de datos..."
+    airflow db init
+else
+    echo "===> airflow.db ya existe, omitiendo inicialización"
+fi
 
-# Crea usuario admin (si ya existe, ignora el error)
-airflow users create \
-  --username admin \
-  --firstname Admin \
-  --lastname User \
-  --role Admin \
-  --email admin@example.com \
-  --password admin || true
+# Crea usuario admin (si ya existe, saltar)
+if ! airflow users list | grep -q 'admin'; then
+  airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+fi
 
 # Arranca scheduler + webserver
 exec airflow scheduler & exec airflow webserver
